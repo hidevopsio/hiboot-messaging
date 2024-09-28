6afc5404df3c643c92df1ee2e7be83160771034a
admin <admin@titanide.cn>
2024-09-28 21:18:07 +0800

    fixes the issue that publish message only received by one of subscribers

--- pubsub/channel.go
+++ pubsub/channel.go
@@ -7,19 +7,21 @@ import (
 
 type ChannelPubSub[T any] struct {
 	mu     sync.RWMutex
-	topics map[string]chan T
+	topics map[string][]chan T
 }
 
 func NewChannelPubSub[T any]() *ChannelPubSub[T] {
-	return &ChannelPubSub[T]{topics: make(map[string]chan T)}
+	return &ChannelPubSub[T]{topics: make(map[string][]chan T)}
 }
 
 func (p *ChannelPubSub[T]) Publish(ctx context.Context, topic string, msg T) error {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 
-	if ch, ok := p.topics[topic]; ok {
-		ch <- msg
+	if subs, ok := p.topics[topic]; ok {
+		for _, ch := range subs {
+			ch <- msg
+		}
 	}
 	return nil
 }
@@ -28,20 +30,24 @@ func (p *ChannelPubSub[T]) Subscribe(ctx context.Context, topic string) (<-chan 
 	p.mu.Lock()
 	defer p.mu.Unlock()
 
-	if _, ok := p.topics[topic]; !ok {
-		p.topics[topic] = make(chan T)
-	}
+	ch := make(chan T)
+	p.topics[topic] = append(p.topics[topic], ch)
 
-	return p.topics[topic], nil
+	return ch, nil
 }
 
-func (p *ChannelPubSub[T]) Unsubscribe(ctx context.Context, topic string) error {
+func (p *ChannelPubSub[T]) Unsubscribe(ctx context.Context, topic string, ch <-chan T) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 
-	if ch, ok := p.topics[topic]; ok {
-		close(ch)
-		delete(p.topics, topic)
+	if subs, ok := p.topics[topic]; ok {
+		for i, subCh := range subs {
+			if subCh == ch {
+				close(subCh)
+				p.topics[topic] = append(subs[:i], subs[i+1:]...)
+				break
+			}
+		}
 	}
 
 	return nil
